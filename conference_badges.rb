56fa7c26686a6800a03447c7d43fcf0549e5b8fc
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-21 20:43:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,5 +10,7 @@ end
 
 def assign_rooms(names)
   new_array = []
-  names.each_with_index {|name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  names.each_with_index do |name, index|
+    new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
 end
